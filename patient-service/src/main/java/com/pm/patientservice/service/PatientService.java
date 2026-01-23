3bab39c2c3cc613900f30e15eb07da9c8b40a6b5
tusharchaudhari1004 <tushar.chaudhari1004@gmail.com>
2026-01-23 18:46:53 +0530

    Patient Service-till DTO

---
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -0,0 +1,28 @@
+package com.pm.patientservice.service;
+
+import com.pm.patientservice.dto.PatientResponseDTO;
+import com.pm.patientservice.mapper.PatientMapper;
+import com.pm.patientservice.model.Patient;
+import com.pm.patientservice.repository.PatientRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PatientService {
+
+    private PatientRepository patientRepository;
+
+    public PatientService(PatientRepository patientRepository){
+        this.patientRepository = patientRepository;
+    }
+
+    public List<PatientResponseDTO> getPatients (){
+        List<Patient> patients = patientRepository.findAll();
+
+        List<PatientResponseDTO> patientResponseDTOS= patients.stream()
+                .map(patient -> PatientMapper.toDTO(patient)).toList();
+
+    }
+
+}
